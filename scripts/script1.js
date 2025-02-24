fa5320bbfa4dcf80d816c4a18f204351f032fe38
Sofiaromo87 <sofia.romo@uabc.edu.mx>
2025-02-23 22:13:11 -0800

    primera version

---
+++ scripts/script1.js
@@ -0,0 +1,29 @@
+// keyword variableName = value;
+// var, let, const
+
+// String
+var firstName = "Sofia";
+var lastName = "Romo";
+
+// concatenation
+console.log("Name: " + firstName + " " + lastName);
+
+firstName = "Fernanda";
+
+// numerical
+let myAge = 99;
+let myHeight = 100;
+
+console.log(myAge, myHeight);
+
+// boolean (true/false)
+let isStudent = true;
+
+// math operations + - / *
+let num1 = 10;
+let num2 = 12;
+
+console.log("El resultado es = " + (num1 + num2));
+console.log("El resultado es = " + (num1 - num2));
+console.log("El resultado es = " + (num1 * num2));
+console.log("El resultado es = " + (num1 / num2).toFixed(3));
